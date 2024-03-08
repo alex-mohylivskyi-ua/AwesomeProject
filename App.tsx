5d9b02cada0682239700b91183d8e7a05e9c3184
Alex Mohylivskyi <alex.mohylivskyi.ua@gmail.com>
2024-03-08 16:14:15 +0200

    React navigation installation

--- App.tsx
+++ App.tsx
@@ -2,11 +2,14 @@ import React, { useState } from 'react';
 import ChoseCorrectAnswer from './components/ChoseCorrectAnswer';
 import store from './app/store';
 import { Provider } from 'react-redux';
+import {NavigationContainer} from '@react-navigation/native';
 
 const App = () => {
   return (
     <Provider store={store}>
-      <ChoseCorrectAnswer />
+      <NavigationContainer>
+        <ChoseCorrectAnswer />
+      </NavigationContainer>
     </Provider>
   );
 };
